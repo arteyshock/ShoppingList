f20692d3a622696d9e2605ffbdc8a10465f02a1b
Art Savin <art.an.savin@gmail.com>
2022-07-14 15:08:31 +0300

    initial commit

---
+++ app/src/main/java/com/artsavin/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.artsavin.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artsavin.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
