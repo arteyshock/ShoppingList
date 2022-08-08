0b13d3da6e3708b60b7cdfa56e3129d246007906
Art Savin <art.an.savin@gmail.com>
2022-08-08 18:38:58 +0300

    + viewModelScope

--- app/src/main/java/com/artsavin/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/artsavin/shoppinglist/presentation/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.artsavin.shoppinglist.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.artsavin.shoppinglist.data.ShopListRepositoryImpl
 import com.artsavin.shoppinglist.domain.DeleteShopItemUseCase
 import com.artsavin.shoppinglist.domain.EditShopItemUseCase
@@ -21,25 +22,18 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(shopListRepository)
     private val editShopItemUseCase = EditShopItemUseCase(shopListRepository)
 
-    private val scope = CoroutineScope(Dispatchers.IO)
-
     val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
-        scope.launch {
+        viewModelScope.launch {
             deleteShopItemUseCase.deleteShopItem(shopItem)
         }
     }
 
     fun toggleShopItem(shopItem: ShopItem) {
-        scope.launch {
+        viewModelScope.launch {
             val newShopItem = shopItem.copy(enabled = !shopItem.enabled)
             editShopItemUseCase.editShopItem(newShopItem)
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        scope.cancel()
     }
 }
